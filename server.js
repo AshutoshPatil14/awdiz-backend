60344d2323d1ec55dc20fabf299b874dfa7bdd6f
Ashutosh Patil <123103890+AshutoshPatil14@users.noreply.github.com>
2025-10-16 10:24:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,10 @@ app.get("/", (req, res) => {
   res.send(`Hello, This is ${process.env.MY_NAME}!`);
 });
 
-app.get("/greet", (req, res) => {
+app.post("/greet", (req, res) => {
   res.send("Hello, Ashutosh Patil");
 });
 
 app.listen(3000, () => {
   console.log("The server is running on port 3000");
 });
-
