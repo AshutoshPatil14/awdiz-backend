7dba6a0f87227189f11bce41a4ad8d1a84a68201
Ashutosh Patil <123103890+AshutoshPatil14@users.noreply.github.com>
2025-10-17 13:37:13 +0530

    Practice

--- server.js
+++ server.js
@@ -1,16 +1,44 @@
 import express from "express";
 import dotenv from "dotenv";
 
-
 const app = express();
 dotenv.config();
+app.use(express.json());
+
+var users = [];
 
 app.get("/", (req, res) => {
   res.send(`Hello, This is ${process.env.MY_NAME}!`);
 });
 
-app.post("/greet", (req, res) => {
-  res.send("Hello, Ashutosh Patil");
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.send("Please provide email and password");
+  }
+  const user = users.find((user) => user.email === email);
+  if (!user) {
+    return res.send("User not found");
+  }
+  if (user.password !== password) {
+    return res.send("Incorrect password");
+  }
+  console.log(req.body, "req");
+  res.send("Hello, You're logged in!");
+});
+
+app.post("/reg", (req, res) => {
+  const existingUser = users.find((user) => user.email === req.body.email);
+  if (existingUser) {
+    return res.send("User already exists");
+  }
+  console.log(req.body, "req");
+  users.push(req.body);
+  res.send("Thanks for registering!\nYou can now log in.");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
 app.listen(3000, () => {
