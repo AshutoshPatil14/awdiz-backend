9d98ee2776cb151aadc01cdd90fdd5445922d10d
Ashutosh Patil <123103890+AshutoshPatil14@users.noreply.github.com>
2025-10-18 23:36:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -47,6 +47,43 @@ app.get("/users", (req, res) => {
   res.send(users);
 });
 
+var newUsers = [
+  { id: 1, name: "Ted", age: 28 },
+  { id: 2, name: "Marshall", age: 32 },
+  { id: 3, name: "Robin", age: 30 },
+];
+
+app.get("/new-users", (req, res) => {
+  res.send(newUsers);
+})
+
+app.patch("/new-users/:id", (req, res) => {
+  const userId = parseInt(req.params.id)
+  const {name, age} = req.body
+  const user = newUsers.find(user => user.id === userId)
+  if (!user) {
+    return res.status(404).send("User not found")
+  }
+  if (name) {
+    user.name = name
+  }
+  if (age) {
+    user.age = age
+  }
+  res.send("User updated successfully")
+})
+
+app.delete("/new-users/:id", (req, res) => {
+  const userId = parseInt(req.params.id)
+  const userIndex = newUsers.findIndex(user => user.id === userId)
+  if (userIndex === -1) {
+    return res.status(404).send("User not found")
+  }
+  newUsers.splice(userIndex, 1)
+  res.send("User deleted successfully")
+})
+
+
 app.listen(3000, () => {
   console.log("The server is running on port 3000");
 });
