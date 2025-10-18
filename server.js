3fe14814524cc4ee2b14bbf04cc1721a5f5db689
Ashutosh Patil <123103890+AshutoshPatil14@users.noreply.github.com>
2025-10-18 11:55:09 +0530

    update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 var users = [];
 
@@ -28,13 +29,18 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/reg", (req, res) => {
-  const existingUser = users.find((user) => user.email === req.body.email);
+  const { email, password } = req.body || {};
+  if (!email || !password) {
+    return res.status(400).send("Please provide email and password");
+  }
+  const existingUser = users.find((user) => user.email === email);
   if (existingUser) {
-    return res.send("User already exists");
+    return res.status(409).send("User already exists");
   }
   console.log(req.body, "req");
-  users.push(req.body);
-  res.send("Thanks for registering!\nYou can now log in.");
+  // Store only the minimal user info for this example
+  users.push({ email, password });
+  res.status(201).send("Thanks for registering!\nYou can now log in.");
 });
 
 app.get("/users", (req, res) => {
