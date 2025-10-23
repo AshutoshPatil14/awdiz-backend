ca1d31ec30d8c964408acd7ec90c7c32911d0b6e
Ashutosh Patil <123103890+AshutoshPatil14@users.noreply.github.com>
2025-10-23 20:26:17 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
-import userRoutes from "./user.actions.js"
-
+import userRoutes from "./user.actions.js";
 
 const mainRouter = Router();
 
-mainRouter.use("/auth", authRoutes)
-mainRouter.use("/user", userRoutes)
-
-
-
-
-
-
+mainRouter.use("/auth", authRoutes);
+mainRouter.use("/user", userRoutes);
 
 export default mainRouter;
