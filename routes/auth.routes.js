c345c14d7cc6d7a89d0c45a365723a15a5e5a542
Ashutosh Patil <123103890+AshutoshPatil14@users.noreply.github.com>
2025-10-29 16:34:16 +0530

    Database Connection

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,38 +1,12 @@
 import express, { Router } from "express";
+import { Login, Register } from "../controllers/auth.controller.js";
 
 const route = Router();
 
 route.use(express.json());
 
-route.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.send("Please provide email and password");
-  }
-  const user = users.find((user) => user.email === email);
-  if (!user) {
-    return res.send("User not found");
-  }
-  if (user.password !== password) {
-    return res.send("Incorrect password");
-  }
-  console.log(req.body, "req");
-  res.send("Hello, You're logged in!");
-});
+route.post("/login", Login);
 
-route.post("/reg", (req, res) => {
-  const { email, password } = req.body || {};
-  if (!email || !password) {
-    return res.status(400).send("Please provide email and password");
-  }
-  const existingUser = users.find((user) => user.email === email);
-  if (existingUser) {
-    return res.status(409).send("User already exists");
-  }
-  console.log(req.body, "req");
-  // Store only the minimal user info for this example
-  users.push({ email, password });
-  res.status(201).send("Thanks for registering!\nYou can now log in.");
-});
+route.post("/reg", Register);
 
 export default route;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mainRouter from "./routes/index.js";
+import mongoose from 'mongoose';
 
 const app = express();
 dotenv.config();
@@ -12,6 +13,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1", mainRouter);
+
+mongoose.connect(process.env.MONGODB_URL).then(() => console.log(`Hello ${process.env.MY_NAME}, Connected to the DB!`));
 
 app.listen(3000, () => {
   console.log("The server is running on port 3000");
